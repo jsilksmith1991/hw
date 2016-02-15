682595918ea2c5d0266be5e10177e004c76e244f
YOUR NAME <jsilksmith@Jeremys-MacBook-Pro.local>
2016-02-15 01:07:44 -0500

    assingment

---
+++ myjavascript.js
@@ -0,0 +1,13 @@
+var A = 100;
+var B = 200;
+
+var addition = A + B;
+var subtraction = A - B;
+var multiply = A * B;
+var divide = A / B;
+
+
+document.getElementById("addition").innerHTML= "Adding " +A+ " and " +B+ " it equals: " +addition;
+document.getElementById("subtraction").innerHTML= "Subtracting" +A+ " and " +B+ " it equals: " +subtraction;
+document.getElementById("multiply").innerHTML= "Multiplying" +A+ " and " +B+ " it equals: " +multiply;
+document.getElementById("division").innerHTML= "Dividing" +A+ " and " +B + " it equals: " +divide; 
